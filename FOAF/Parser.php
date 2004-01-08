ebc759afc5a1ccab5c650fee723458ea208da18e
Davey Shafik <davey@php.net>
2004-01-08 07:37:17 +0000

    Started using RDQL, going to move to a different approach. Commiting for recoverys sake
    git-svn-id: http://svn.php.net/repository/pear/packages/XML_FOAF/trunk@148148 c90b9560-bf6c-de11-be94-00142212c4b1

--- FOAF/Parser.php
+++ FOAF/Parser.php
@@ -35,33 +35,45 @@
  * @todo Implement PEAR_Error handling
  */
 
-define('RDFAPI_INCLUDE_DIR', './RAP/');
+define('RDFAPI_INCLUDE_DIR', '../../RAP/');
 
 class XML_FOAF_Parser {
 	
 		function XML_FOAF_Parser() {
         	require_once RDFAPI_INCLUDE_DIR . 'RdfAPI.php';
         	$this->rdf_parser =& new RdfParser;
-        	$this->rdf_model =& $this->rdf_parser->generateModel($this->annotation);
-        	$rdql = 'SELECT ?a ?body WHERE (?a <rdf:type> <a:Annotation>), (?a <a:body> ?body) USING a FOR <http://www.w3.org/2000/10/annotation-ns#>, rdf FOR <http://www.w3.org/1999/02/22-rdf-syntax-ns#>';
-        	$result = $this->rdf_model->rdqlquery($rdql,TRUE);
-        	RdqlEngine::writeQueryResultAsHtmlTable($result);
 		}
 		
 		function parseFromURI($uri) {
-			
+			$this->parseFromFile($uri);
 		}
 		
 		function parseFromFile($file) {
-			
+			$this->foaf = file_get_contents($file);
+			$this->_parse();
 		}
 		
 		function parseFromMem($mem) {
-			
+			$this->foaf = $mem;
+			$this->_parse();
 		}
 		
 		function _parse() {
-			
+			$this->rdf_model =& $this->rdf_parser->generateModel($this->foaf);
+        	$rdql = 'SELECT '
+        				. '?foaf '
+        				. 'WHERE '
+						. '(?foaf <rdf:type> <foaf:Person>) '
+						. 'USING '
+						. 'foaf FOR <http://xmlns.com/foaf/0.1/>'
+						. ', rdf FOR <http://www.w3.org/1999/02/22-rdf-syntax-ns#>';
+        	$result = $this->rdf_model->rdqlquery($rdql,TRUE);
+			if (is_a($result[0]['?foaf'], 'blanknode')) {
+				$this->foaf_type = 'Person';
+			} 
+        	RdqlEngine::writeQueryResultAsHtmlTable($result);
+        	$this->rdf_model->writeAsHTMLTable();
+        	echo '<pre>' . htmlentities($this->rdf_model->writeRdfToString()) . '</pre>';
 		}
 		
 		function toObject() {
