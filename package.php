66a002d7c43a452cd96391ef3e19643dac06f45d
Lukas Smith <lsmith@php.net>
2004-11-04 13:32:38 +0000

    start 0.3.0 release cycle
    git-svn-id: http://svn.php.net/repository/pear/packages/XML_FOAF/trunk@172099 c90b9560-bf6c-de11-be94-00142212c4b1

--- package.php
+++ package.php
@@ -2,17 +2,15 @@
 require_once 'PEAR/PackageFileManager.php';
 require_once 'Console/Getopt.php';
 
-$version = '0.2';
+$version = '0.3.0';
 $notes = <<<EOT
-	* Added XML_FOAF_Parser.
-    * Fixed small bugs in XML_FOAF
-    * Moved common methods to XML_FOAF_Common
+* Moved to PEAR::RDF as the RDF backend
 EOT;
 
 $description =<<<EOT
-	XML_FOAF Allows advanced creation and simple parsing of FOAF RDF/XML files.
-    The FOAF Project can be found at http://www.foaf-project.org -
-    XML_FOAF_Lite will soon follow for simple creation of FOAFs.
+XML_FOAF Allows advanced creation and simple parsing of FOAF RDF/XML files.
+The FOAF Project can be found at http://www.foaf-project.org -
+XML_FOAF_Lite will soon follow for simple creation of FOAFs.
 EOT;
 
 $package = new PEAR_PackageFileManager();
@@ -25,7 +23,7 @@ $result = $package->setOptions(array(
     'state'             => 'alpha',
     'license'           => 'PHP License',
     'filelistgenerator' => 'cvs',
-    'ignore'            => array('package.php', 'package.xml','*test*','*RAP.bak*'),
+    'ignore'            => array('package.php', 'package.xml'),
     'notes'             => $notes,
     'changelogoldtonew' => false,
     'baseinstalldir'    => 'XML',
@@ -47,7 +45,7 @@ $package->addDependency('php', '4.3.0', 'ge', 'php', false);
 $package->addDependency('XML_Tree', '1.1', 'ge', 'pkg', false);
 $package->addDependency('XML_Beautifier', '0.2.2', 'ge', 'pkg', false);
 
-if ($_SERVER['argv'][1] == 'commit') {
+if (isset($_GET['make']) || (isset($_SERVER['argv'][1]) && $_SERVER['argv'][1] == 'make')) {
     $result = $package->writePackageFile();
 } else {
     $result = $package->debugPackageFile();
